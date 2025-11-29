56fe03c9d3c4317c8f792080079fd87ce5d7e3f0
Maxwell Sule <sule.maxwell7@gmail.com>
2025-11-29 18:33:22 -0500

    new

---
+++ src/file21_new.java
@@ -0,0 +1,14 @@
+public class file21_old {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+        int prod = a * b;
+        System.out.println("Product: " + prod);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Loop: " + i);
+        }
+        System.out.println("Done");
+    }
+}
